2f9e5dd869264383891a48967d8de2117c34b505
Bharatkumar17 <70887566+Bharatkumar17@users.noreply.github.com>
2025-05-29 13:40:15 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,3 +3,5 @@ from os import environ
 API_ID = int(environ.get("API_ID", "26729193"))
 API_HASH = environ.get("API_HASH", "a94598ef642481e35466292df95f251e")
 BOT_TOKEN = environ.get("BOT_TOKEN", "1322869542:AAE4hEa0IrOotic5OO3m_iZNwzWK_tdGYfA")
+# For example, if using UptimeRobot:
+PING_URL = "https://your-bot-url.herokuapp.com/"
